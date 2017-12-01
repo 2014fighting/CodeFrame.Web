5dc6395ea02b22fb80220b378b5c600dc0ffe886
余文清 <ywq19920427@163.com>
2017-12-01 10:58:03 +0800

    dispose测试

---
+++ CodeFrame.XUnit/DisposeTest.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace CodeFrame.XUnit
+{
+   
+    public class DisposeTest
+    {
+        [Fact]
+        public void Mytest()
+        {
+            using (var x = new MyDisposable())
+            {
+                
+            }
+            
+
+        }
+    }
+
+    public class MyDisposable : IDisposable
+    {
+        public MyDisposable()
+        {
+            Console.WriteLine("+ {0} was created", this.GetType().Name);
+        }
+
+        public void Dispose()
+        {
+            Console.WriteLine("- {0} was disposed!", this.GetType().Name);
+        }
+    }
+}
